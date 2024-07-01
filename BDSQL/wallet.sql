8ab5d197deb4e84da37b321aebd7b28b08aa182d
JorgeLeal1 <68608190+JorgeLeal1@users.noreply.github.com>
2024-07-01 02:36:02 -0400

    Actualizacion de BD

--- BDSQL/wallet.sql
+++ BDSQL/wallet.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 27-06-2024 a las 07:44:56
+-- Tiempo de generación: 01-07-2024 a las 08:34:57
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
@@ -41,7 +41,8 @@ CREATE TABLE `cliente` (
 
 INSERT INTO `cliente` (`run`, `nombre1`, `nombre2`, `appaterno`, `apmaterno`) VALUES
 ('16286667-2', 'Claudia', 'Maria', 'Hope', 'Fierro'),
-('16330225-k', 'pedro', 'carlos', 'Carmelo', 'Cuevas');
+('16330225-k', 'pedro', 'carlos', 'Carmelo', 'Cuevas'),
+('23357740-5', 'pedro', 'prueba', 'este', 'otro');
 
 -- --------------------------------------------------------
 
@@ -62,8 +63,9 @@ CREATE TABLE `cuenta` (
 --
 
 INSERT INTO `cuenta` (`nroCuenta`, `alias`, `banco`, `saldo`, `run_cliente`) VALUES
-(5, 'LaClaudia', 'Banco de Chile', 150005, '16286667-2'),
-(37, 'ElPedrox', 'Banco Estado', 430, '16330225-k');
+(5, 'LaClaudia', 'Banco de Chile', 400, '16286667-2'),
+(16, 'josex', 'Masterplop', 1, '23357740-5'),
+(37, 'ElPedrox', 'Banco Estado', 85, '16330225-k');
 
 -- --------------------------------------------------------
 
@@ -84,7 +86,9 @@ CREATE TABLE `cuentatercero` (
 --
 
 INSERT INTO `cuentatercero` (`idCuentaTercero`, `nroCuentaTercero`, `run_clienteCuentaTercero`, `nroCuentaOrigen`, `run_clienteOrigen`) VALUES
-(8, 5, '16286667-2', 37, '16330225-k');
+(8, 5, '16286667-2', 37, '16330225-k'),
+(9, 37, '16330225-k', 5, '16286667-2'),
+(10, 37, '16330225-k', 16, '23357740-5');
 
 -- --------------------------------------------------------
 
@@ -146,9 +150,33 @@ CREATE TABLE `transaccion` (
 --
 
 INSERT INTO `transaccion` (`id`, `idUsuarioEnvia`, `idUsuarioRecibe`, `saldo`, `date`, `idMoneda`, `idTipoTransaccion`) VALUES
-(28, 2, 2, 1, '2024-06-27 01:11:21', 1, 1),
+(28, 1, 2, 1, '2024-06-26 01:11:21', 1, 1),
 (29, 2, 2, 390, '2024-06-27 01:14:15', 1, 2),
-(30, 2, 2, 150000, '2024-06-27 01:43:57', 1, 1);
+(30, 2, 1, 150000, '2024-06-28 01:43:57', 1, 3),
+(31, 1, 1, 1, '2024-06-28 22:09:02', 1, 1),
+(32, 1, 1, 23, '2024-06-28 22:12:57', 1, 1),
+(33, 1, 1, 15, '2024-06-28 22:13:25', 1, 1),
+(34, 1, 1, 2, '2024-06-28 22:13:28', 1, 1),
+(35, 1, 1, 13, '2024-06-28 22:13:32', 1, 1),
+(36, 1, 1, 10, '2024-06-28 22:13:35', 1, 1),
+(37, 1, 2, 300, '2024-06-28 22:23:21', 1, 3),
+(38, 1, 2, 200, '2024-06-28 22:24:29', 1, 3),
+(39, 1, 1, 20, '2024-06-29 00:16:52', 1, 1),
+(40, 1, 1, 15, '2024-06-29 00:17:05', 1, 2),
+(41, 1, 1, 10, '2024-06-29 00:20:20', 1, 1),
+(42, 1, 1, 50, '2024-06-29 00:20:25', 1, 2),
+(43, 2, 1, 100, '2024-06-29 00:38:19', 1, 3),
+(44, 1, 1, 1, '2024-06-29 23:59:46', 1, 1),
+(45, 1, 1, 2, '2024-06-29 23:59:48', 1, 1),
+(46, 1, 1, 2, '2024-06-29 23:59:51', 1, 2),
+(47, 1, 1, 1, '2024-06-30 15:08:50', 1, 1),
+(48, 1, 1, 2, '2024-06-30 15:08:54', 1, 2),
+(49, 1, 2, 300, '2024-06-30 15:11:10', 1, 3),
+(50, 7, 7, 1, '2024-06-30 21:04:25', 1, 1),
+(51, 7, 7, 20, '2024-06-30 21:04:41', 1, 1),
+(52, 7, 7, 20, '2024-06-30 21:04:57', 1, 2),
+(53, 7, 7, 20, '2024-06-30 21:05:22', 1, 1),
+(54, 7, 1, 20, '2024-06-30 21:34:01', 1, 3);
 
 -- --------------------------------------------------------
 
@@ -169,8 +197,9 @@ CREATE TABLE `usuario` (
 --
 
 INSERT INTO `usuario` (`id`, `nombre`, `correo_electronico`, `contrasena`, `run_cliente`) VALUES
-(1, 'pedro.carmelo', 'pedro@mailinator.com', '111', '16330225-k'),
-(2, 'claudia.hope', 'qeziju@mailinator.com', '111', '16286667-2');
+(1, 'pedro.carmelo', 'pedro@mailinator.com', '$2a$10$LrJNieWstwWj055//WGngepln/ajwoJYr88fbjujj30C.DYGGIfE2', '16330225-k'),
+(2, 'claudia.hope', 'qeziju@mailinator.com', '$2a$10$LrJNieWstwWj055//WGngepln/ajwoJYr88fbjujj30C.DYGGIfE2', '16286667-2'),
+(7, 'jose.prueba', 'este.otro@mailinator.com', '$2a$10$gB9z8qz.wwX/6Py/nF1u.e78RoMOrhUscIKTDEXrh75FxhjjyywdK', '23357740-5');
 
 --
 -- Índices para tablas volcadas
@@ -235,7 +264,7 @@ ALTER TABLE `usuario`
 -- AUTO_INCREMENT de la tabla `cuentatercero`
 --
 ALTER TABLE `cuentatercero`
-  MODIFY `idCuentaTercero` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `idCuentaTercero` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT de la tabla `moneda`
@@ -253,13 +282,13 @@ ALTER TABLE `tipotransaccion`
 -- AUTO_INCREMENT de la tabla `transaccion`
 --
 ALTER TABLE `transaccion`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=55;
 
 --
 -- AUTO_INCREMENT de la tabla `usuario`
 --
 ALTER TABLE `usuario`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- Restricciones para tablas volcadas
